9c182bc01a6389e2c4feed048726398e73decaa0
Shubham7270 <116951620+Shubham7270@users.noreply.github.com>
2023-01-13 20:51:31 +0530

    Day 4 Assignment Submitted...
    In this Assignment  We use DOM. Using DOM We change Background Color of Card.

---
+++ script.js
@@ -0,0 +1,10 @@
+const h1Title = document.getElementById("title");
+
+h1Title.textContent = "Hello Programmer`s... !";
+
+const ParagraphContent = document.getElementById("para");
+ParagraphContent.style.fontSize = "50px";
+ParagraphContent.style.padding = "20px";
+
+const CardContent = document.getElementById("Card");
+CardContent.style.backgroundColor = "#03C988";
